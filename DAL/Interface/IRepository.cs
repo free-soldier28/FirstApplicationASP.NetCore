5652e51121b44942bc29632f7dae69a448b302cf
romik280h <romik280h@gmail.com>
2018-09-03 22:51:08 +0300

    В слой DAL добавил интерфейс IRepository. Добавил слой для репозитория и в нем создал Generic Repository.

---
+++ DAL/Interface/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Interface
+{
+    public interface IRepository<T> : IDisposable
+        where T : class
+    {
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Create(T obj);
+        void Update(T obj);
+        void Delete(int id);
+    }
+}
---
+++ MSSQLRepositiry/Repository.cs
@@ -0,0 +1,119 @@
+﻿using DAL.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class Repository<T> : IRepository<T> where T: class
+    {
+        private DbContext _db;
+        private DbSet<T> _dbSet;
+        private bool disposedValue = false;
+
+        public Repository(DbContext db)
+        {
+            _db = db;
+            _dbSet = db.Set<T>();
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            IEnumerable<T> list = null;
+
+            if (predicate != null)
+            {
+                list = _dbSet.Where(predicate).ToList();
+            }
+            return list;
+        }
+
+        public T GetById(int id)
+        {
+            T obj = null;
+
+            if (id != 0)
+            {
+                obj = _dbSet.Find(id);
+            }
+            return obj;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Create(T obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    _dbSet.Add(obj);
+                    _db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void Update(T obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    _db.Entry(obj).State = EntityState.Modified;
+                    _db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    T obj = _dbSet.Find(id);
+                    if (obj != null)
+                    {
+                        _dbSet.Remove(obj);
+                        _db.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposedValue)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
